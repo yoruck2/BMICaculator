cc0fe7c77f07e265b618ad55797eb3bf0eb005a0
yoruck2 <yoruck2@gmail.com>
2024-05-21 17:37:33 +0900

    Initial Commit

---
+++ BMICaculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BMICaculator
+//
+//  Created by dopamint on 5/21/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
